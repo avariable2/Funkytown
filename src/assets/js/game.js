a50918d4cb84243f32c593e5dd4445d1251a3cb2
thayral <theo.ayral@gmail.com>
2021-09-16 15:01:05 +0200

    added second player : elf

--- src/assets/js/game.js
+++ src/assets/js/game.js
@@ -1,6 +1,7 @@
 class Game extends Phaser.Scene {
     touches;
     player;
+    player2;
 
     constructor() {
         super();
@@ -12,27 +13,42 @@ class Game extends Phaser.Scene {
         'assets/assetsGame/DWARF.png',
         { frameWidth: 31, frameHeight: 31, startFrame: 0, endFrame: 3}
         );
+        this.load.spritesheet('elf',
+        'assets/assetsGame/ELF.png',
+        { frameWidth: 31, frameHeight: 31, startFrame: 0, endFrame: 3}
+        );
         this.load.image('bg', 'assets/assetsGame/bg.jpg');
     }
 
     create () // Ici on ajoute ce que l'on a preload et on modifie pour l'etat de base du jeu
     {
         this.add.image(0,0, 'bg').setOrigin(0,0);
-
         this.anims.create({
             key: 'default', // nom de l'animation
             frames: 'dude', 
-<<<<<<< HEAD
-            frameRate: 3, // vitesse de l'animation
-=======
             frameRate: 5, // vitesse de l'animation
->>>>>>> b6bf0c12629b3a46b932c0414ee978731e60ccf6
+            repeat : -1 // infini
+        });
+        this.anims.create({
+            key: 'default_elf', // nom de l'animation
+            frames: 'elf', 
+            frameRate: 5, // vitesse de l'animation
             repeat : -1 // infini
         });
         
         this.touches = this.input.keyboard.createCursorKeys();
         this.player = this.add.sprite(400, 300, 'dude').play('default');
         this.player.setScale(4);
+
+        this.touches.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.touches.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        this.touches.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.touches.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+
+
+
+        this.player2 = this.add.sprite(400, 300, 'elf').play('default_elf');
+        this.player2.setScale(5);
     }
 
     update () // Ici on s'occupe des interractions
@@ -44,8 +60,19 @@ class Game extends Phaser.Scene {
         } else if(this.input.keyboard.checkDown(this.touches.down, 0)) {
             this.player.y += 5;
         } else if(this.input.keyboard.checkDown(this.touches.up, 0)) {
-            this.player.y -= 5;
+            this.player.y -= 5; 
         } 
+
+        if(this.input.keyboard.checkDown(this.touches.keyA, 0)) {
+            this.player2.x -= 5;
+        } else if(this.input.keyboard.checkDown(this.touches.keyD, 0)) {
+            this.player2.x += 5;
+        } else if(this.input.keyboard.checkDown(this.touches.keyS, 0)) {
+            this.player2.y += 5;
+        } else if(this.input.keyboard.checkDown(this.touches.keyW, 0)) {
+            this.player2.y -= 5;
+        } 
+
     }
 }
 
